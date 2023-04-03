29b378a51cd6d4d9a1852fe40abde1dceca6302b
Eremeev Vlad <eremeev.vlad.v@yandex.ru>
2023-04-03 17:42:25 +0300

    lab1

---
+++ src/main/java/com/example/rsue3labs/Rsue3labsApplication.java
@@ -0,0 +1,13 @@
+package com.example.rsue3labs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Rsue3labsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Rsue3labsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/rsue3labs/Rsue3labsApplicationTests.java
@@ -0,0 +1,27 @@
+package com.example.rsue3labs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@WebMvcTest(HomeController.class)
+class Rsue3labsApplicationTests {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void testHomePage() throws Exception {
+        mockMvc.perform(post("/"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("home"))
+                .andExpect(content().string(
+                        containsString("Welcome to...")
+                ));
+    }
+}
